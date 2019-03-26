48890aeb1fa241ea7e9c5c1bd3fa80c7cf6dc9fa
AnthonyMRios <anthonymrios@gmail.com>
2019-03-26 12:45:32 -0500

    added example for homework

--- misc/functions/exercise.py
+++ misc/functions/exercise.py
@@ -1,6 +1,6 @@
-def my_exercise(sum_or_subtract, numbers):
+def my_exercise(sum_or_subtract, filename):
     '''
-    Sum or Subtract from 0 all the numbers in a ilst
+    Sum or Average all numbers in the file filename
     :param str sum_or_subtract: A string "sum" or "subtract"
     :param list numbers: A list of numbers (floats)
     :return: The float of the sum/subtraction of all numbers from 0
--- misc/functions/main.py
+++ misc/functions/main.py
@@ -1,16 +1,12 @@
+# You should NOT edit this file!
 from exercise import *
 
 def main():
-    my_numbers = []
-    with open('./data/numbers.txt') as in_file:
-        for number in in_file:
-            my_numbers.append(int(number))
-
     arg1 = 'sum'
-    print("SUM: {}".format(my_exercise(arg1, my_numbers)))
+    print("SUM: {}".format(my_exercise("sum", './data/numbers.txt')))
 
     arg1 = 'subtract'
-    print("SUBTRACTION: {}".format(my_exercise(arg1, my_numbers)))
+    print("SUBTRACTION: {}".format(my_exercise("average", './data/numbers.txt')))
 
 if __name__ == '__main__':
     main()
